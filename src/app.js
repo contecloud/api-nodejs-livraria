60d0fdf5862b5fe0209bf87c0fa246640bd63ad5
Carlo Conte <carlo.conte@gmail.com>
2025-10-06 21:32:12 -0300

    bla

--- src/app.js
+++ src/app.js
@@ -1,63 +1,14 @@
 import express from "express";
-import conectaNoDB from "./config/dbconnect.js";
+import db from "./config/dbConnect.js"
+import routes from "./routes/index.js"
 
-const conexao = await conectaNoDB();
-
-conexao.on("error", (erro) => {
-    console.error("Erro de conexão", erro);
-});
-
-conexao.once("open", () => {
-    console.log("Conexão com BD conectado com sucesso!");
-});
+db.on("error", console.log.bind(console, 'Erro de conexão'))
+db.once("open", () => {
+  console.log('conexão com o banco feita com sucesso')
+})
 
 const app = express();
+app.use(express.json())
+routes(app);
 
-app.use(express.json());
-
-const livros = [
-    {
-        id: 1,
-        titulo: "O senhor dos aneis",
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit",
-    },
-
-]
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    })
-}
-
-app.get("/", (req, res) => {
-    res.status(200).send("Curso de node.js");
-});
-
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
-});
-
-app.get("/livros/:id", (req, res) => {
-    res.status(200).json(livros[buscaLivro(req.params.id)]);
-});
-
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("Registro criado");
-});
-
-app.put("/livros/:id", (req, res) => {
-    livros[buscaLivro(req.params.id)].titulo = req.body.titulo;
-    res.status(200).send(livros);
-})
-
-app.delete("/livros/:id", (req, res) => {
-    livros.splice(buscaLivro(req.params.id), 1);
-    res.status(200).send(livros);
-})
-
-export default app; 
+export default app
--- src/config/dbconnect.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-async function conectaNoDB() {    
-    mongoose.connect("mongodb://user:teste123@localhost:27017/");
-
-    return mongoose.connection;
-}
-
-export default conectaNoDB;
